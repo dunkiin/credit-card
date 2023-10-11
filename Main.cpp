b0fc7fec47f92de950aa935014f9b0feba0aaabe
Alfie Sun <alfiesunn666@gmail.com>
2023-10-10 21:55:56 -0700

    Updated for use txt file to do as userInput

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 #include "Credit.h"
+#include <fstream>
 
 #include <string>
 int main() {
-    std::string testCard = "4388576018410707";
 
-    std::cout << "Hello World!\n";
+    std::string inputFile = "cards.txt";
+    std::ifstream fin;
+    fin.open(inputFile);
+
+    std::string userInput;
+    int validSum = 0;
+    int invalidSum = 0;
+
+    if (fin.is_open()) {
+        std::cout << "--------------------------------" << '\n';
+        while (fin >> userInput) {
+            
+            if (isCardValid(userInput)) {
+                std::cout << userInput << "   " << "valid: " << isCardValid(userInput) << '\n';
+                validSum++;
+            }
+            else {
+                std::cout << userInput << "   " << "valid: " << isCardValid(userInput) << '\n';
+                invalidSum++;
+            }
+        }
+        fin.close();
+    }
+    else
+    {
+        std::cout << "Error:Could not open file: \"" << inputFile << '\n';
+    }
+
+    std::cout << "--------------------------------" << '\n';
+    std::cout << "valid cards: " << validSum << '\n';
+    std::cout << "invalid cards: " << invalidSum << '\n';
+    
+    
+
+    //std::cout << "Hello World!\n";
     //std::cout << getSumOfDigits(123) << '\n';
     //std::cout << startsWith(testCard, "34") << '\n';
     //std::cout << hasValidPrefix(testCard) << '\n';
     //std::cout << sumOddDigitsRightToLeft(testCard) << '\n';
-    std::cout << sumEvenDigitsRightToLeft(testCard) << '\n';
-    std::cout << isCardValid(testCard) << '\n';
+    //std::cout << sumEvenDigitsRightToLeft(testCard) << '\n';
+    
     
 }
 
